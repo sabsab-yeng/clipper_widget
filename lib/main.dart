37ae44c092377af5d19a805578466a191c55f59f
sabsab-yeng <sabsabyp@gmail.com>
2020-03-26 22:40:13 +0700

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:clippath_widget/ui/clip_path_widget.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ClipPathWidget(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
